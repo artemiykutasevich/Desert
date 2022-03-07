ac717e1e02b9ce1cc089ef231c8bc33aa32d850b
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-03-07 19:31:45 +0300

    Initial Commit

---
+++ Desert/DesertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertApp.swift
+//  Desert
+//
+//  Created by Artem Kutasevich on 7.03.22.
+//
+
+import SwiftUI
+
+@main
+struct DesertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
